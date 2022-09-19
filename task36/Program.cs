15d2f550f45158051aa8370bff108810bf768b67
Vladimir Kharlampidi <harlamp_83@yahoo.com>
2022-09-19 22:21:28 +0300

    Найдите сумму

---
+++ task36/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int size = 20;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    int sum = 0;
+    Console.WriteLine();
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(arr[i] + " ");
+        if (i % 2 > 0) sum += arr[i];
+    }
+    Console.WriteLine($"Сумма чисел, стоящих на нечетной позиции, соcтаваляет {sum} ");
+}
+
+PrintArray(FillArray(size));
