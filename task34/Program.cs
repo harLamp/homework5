58a3686a93338c782ada35a43b213c43cebd3c0f
Vladimir Kharlampidi <harlamp_83@yahoo.com>
2022-09-19 22:12:31 +0300

    Заполнил массив

---
+++ task34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int size = 20;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintResult(int[] array)
+{
+    int even = 0;
+    int uneven = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+        if (array[i] % 2 == 0) even++;
+        else uneven++;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Количество четных чисел {even}, количество нечетных чисел {uneven}");
+    Console.WriteLine();
+}
+PrintResult(FillArray(size));
